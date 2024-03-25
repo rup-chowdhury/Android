499b7fa2ea28a87d2511f042f37ca7f786284fe6
Rup Chowdhury <rupbd1@gmail.com>
2024-03-25 23:47:48 +0600

    Update MainActivity.java

--- PracticeThree/app/src/main/java/com/example/practicethree/MainActivity.java
+++ PracticeThree/app/src/main/java/com/example/practicethree/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.practicethree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOne = findViewById(R.id.button_one);
+        btnTwo = findViewById(R.id.button_two);
+        btnThree = findViewById(R.id.button_three);
+        btnFour = findViewById(R.id.button_four);
+        btnFive = findViewById(R.id.button_five);
+        btnSix = findViewById(R.id.button_six);
+        btnSeven = findViewById(R.id.button_seven);
+        btnEight = findViewById(R.id.button_eight);
+        btnNine = findViewById(R.id.button_nine);
+        btnZero = findViewById(R.id.button_zero);
     }
 }
