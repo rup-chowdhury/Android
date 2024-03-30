2ccd3a7be107c35b8861ff564e898257d059f68e
Rup Chowdhury <rupbd1@gmail.com>
2024-03-30 13:39:25 +0600

    a

--- PracticeThree/app/src/main/java/com/example/practicethree/SplashScreen.java
+++ PracticeThree/app/src/main/java/com/example/practicethree/SplashScreen.java
@@ -1,14 +1,34 @@
 package com.example.practicethree;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ConstraintLayout actSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        actSplash = findViewById(R.id.act_splash);
+
+        actSplash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+
+                startActivity(i);
+
+                //startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
     }
+}
 }
