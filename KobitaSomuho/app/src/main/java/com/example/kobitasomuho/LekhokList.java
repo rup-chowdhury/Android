a4ff7b00c9a636e312e9974319ca1cef2079b654
Rup Chowdhury <rupbd1@gmail.com>
2024-03-30 21:55:46 +0600

    a

--- KobitaSomuho/app/src/main/java/com/example/kobitasomuho/LekhokList.java
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/LekhokList.java
@@ -3,6 +3,7 @@ package com.example.kobitasomuho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class LekhokList extends AppCompatActivity {
 
@@ -10,10 +11,18 @@ public class LekhokList extends AppCompatActivity {
 
     String[] dob;
 
+    ListView
+
     int[] images = {R.drawable.mujtaba, R.drawable.manik, R.drawable.satyajit, R.drawable.siraj, R.drawable.bivuti};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lekhok_list);
+
+        lstKobis = findViewById(R.id.lst_kobi);
+
+        kobis = getResources().getStringArray(R.array.kobis);
+
+        dob = getResources().getStringArray(R.array.dob);
     }
 }
