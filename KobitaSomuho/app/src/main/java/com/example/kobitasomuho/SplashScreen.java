702dc50ca1d8cd31d48821e38b2628a5c9ddf5c3
Rup Chowdhury <rupbd1@gmail.com>
2024-04-02 17:20:03 +0600

    Update SplashScreen.java

--- KobitaSomuho/app/src/main/java/com/example/kobitasomuho/SplashScreen.java
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/SplashScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        Log.e("Rup","onCreate");
         new Handler().postDelayed(new Runnable() {  //Delay Timer on Splash screen
             @Override
             public void run() {
@@ -21,4 +23,19 @@ public class SplashScreen extends AppCompatActivity {
             }
         },2000);
         }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
