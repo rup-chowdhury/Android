2dd48624e521ae534155cbc94714cb15d9c7ff85
Rup Chowdhury <rupbd1@gmail.com>
2024-04-03 21:36:05 +0600

    Update Golpo.java

--- KobitaSomuho/app/src/main/java/com/example/kobitasomuho/Golpo.java
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/Golpo.java
@@ -2,10 +2,8 @@ package com.example.kobitasomuho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -28,66 +26,6 @@ public class Golpo extends AppCompatActivity {
 
         golpoName = getResources().getStringArray(R.array.golponame);
 
-        listGolpo.setAdapter(new ListAdapter() {
-            @Override
-            public boolean areAllItemsEnabled() {
-                return false;
-            }
-
-            @Override
-            public boolean isEnabled(int position) {
-                return false;
-            }
-
-            @Override
-            public void registerDataSetObserver(DataSetObserver observer) {
-
-            }
-
-            @Override
-            public void unregisterDataSetObserver(DataSetObserver observer) {
-
-            }
-
-            @Override
-            public int getCount() {
-                return 0;
-            }
-
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                return null;
-            }
-
-            @Override
-            public int getItemViewType(int position) {
-                return 0;
-            }
-
-            @Override
-            public int getViewTypeCount() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-        });
+        listGolpo.setAdapter(new ArrayAdapter<String>(Golpo.this, android.R.layout.simple_list_item_1, listGolpo));
     }
 }
