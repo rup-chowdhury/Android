3b42f4a501d5d60f79f63436df99cbd5c28c4c47
Rup Chowdhury <rupbd1@gmail.com>
2024-04-03 23:13:49 +0600

    a

--- KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoMujtaba1.java
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoMujtaba1.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GolpoMujtaba extends AppCompatActivity {
+public class GolpoMujtaba1 extends AppCompatActivity {
 
+    String[] golpo1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_golpo_mujtaba);
+        setContentView(R.layout.activity_golpo_mujtaba_one);
+
+
+
     }
 }
--- KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoNameMujtaba.java
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoNameMujtaba.java
@@ -2,9 +2,10 @@ package com.example.kobitasomuho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.LauncherActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,7 +30,16 @@ public class GolpoNameMujtaba extends AppCompatActivity {
 
         listGolpo.setAdapter(new ArrayAdapter<String>(GolpoNameMujtaba.this, android.R.layout.simple_list_item_1, golpoName));
 
-        
-        startActivity(new Intent(GolpoNameMujtaba.this, GolpoMujtaba.class));
+        listGolpo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                if (position == 0){
+                    startActivity(new Intent(GolpoNameMujtaba.this, GolpoMujtaba1.class));
+                }
+            }
+        });
+
     }
 }
