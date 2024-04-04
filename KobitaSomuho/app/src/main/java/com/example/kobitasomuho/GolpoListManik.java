297692d80bf6dc474084efb5f01ddc5e1c6c7ec2
Rup Chowdhury <rupbd1@gmail.com>
2024-04-04 16:25:03 +0600

    a

--- KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoListManik.java
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoListManik.java
@@ -3,12 +3,19 @@ package com.example.kobitasomuho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class GolpoListManik extends AppCompatActivity {
+
+    String[] golpoName;
+
+    ListView lstGolpo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_golpo_list_manik);
+
+        lstGolpo = findViewById(R.id.lst_golpo_manik);
     }
 }
