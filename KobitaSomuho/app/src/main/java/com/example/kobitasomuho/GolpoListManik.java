334d9f760dee5f0367b18366d089cdf657bfbc69
Rup Chowdhury <rupbd1@gmail.com>
2024-04-04 16:18:56 +0600

    a

---
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoListManik.java
@@ -0,0 +1,14 @@
+package com.example.kobitasomuho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GolpoListManik extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_golpo_list_manik);
+    }
+}
