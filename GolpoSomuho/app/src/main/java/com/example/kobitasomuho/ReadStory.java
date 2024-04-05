9ccb0fba5f1272f02ca765aed2e2fd91ec3196ea
Rup Chowdhury <rupbd1@gmail.com>
2024-04-05 14:47:26 +0600

    a

--- GolpoSomuho/app/src/main/java/com/example/kobitasomuho/ReadStory.java
+++ GolpoSomuho/app/src/main/java/com/example/kobitasomuho/ReadStory.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GolpoMujtaba1 extends AppCompatActivity {
+public class ReadStory extends AppCompatActivity {
 
-    String[] golpo1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_golpo_mujtaba_one);
+        setContentView(R.layout.activity_read_story);
     }
 }
