1e9a05059aa59e6ebeb81137fcd416193ecf027e
Rup Chowdhury <rupbd1@gmail.com>
2024-04-06 17:19:13 +0600

    a

--- KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoListSatyajit.java
+++ KobitaSomuho/app/src/main/java/com/example/kobitasomuho/GolpoListSatyajit.java
@@ -1,4 +1,14 @@
 package com.example.kobitasomuho;
 
-public class GolpoListSatyajit {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GolpoListSatyajit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_golpo_list_satyajit);
+    }
 }
