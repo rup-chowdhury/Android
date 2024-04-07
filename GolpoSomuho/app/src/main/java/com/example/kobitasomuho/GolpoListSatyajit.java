bbde5f15a341dae443b5b7ac78ef7933a7a54a8f
Rup Chowdhury <rupbd1@gmail.com>
2024-04-07 13:23:30 +0600

    a

--- GolpoSomuho/app/src/main/java/com/example/kobitasomuho/GolpoListSatyajit.java
+++ GolpoSomuho/app/src/main/java/com/example/kobitasomuho/GolpoListSatyajit.java
@@ -3,12 +3,20 @@ package com.example.kobitasomuho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class GolpoListSatyajit extends AppCompatActivity {
 
+    String[] golpoList;
+
+    ListView lstGolpo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_golpo_list_satyajit);
+
+        lstGolpo = findViewById(R.id.lst_satyajit);
+
+        golpoList = getResources().getStringArray(R.string.golponamesatyajit1);
     }
 }
