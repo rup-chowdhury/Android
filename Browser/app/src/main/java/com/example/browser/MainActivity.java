cb22c9c6f901ecb4d8df527d8b7794a651fcc79a
Rup Chowdhury <rupbd1@gmail.com>
2024-04-21 18:04:38 +0600

    Webview variable and connect Interface

--- Browser/app/src/main/java/com/example/browser/MainActivity.java
+++ Browser/app/src/main/java/com/example/browser/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.browser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    WebView Browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Browser = findViewById(R.id.web_browser);
+        Browser.loadUrl("https://www.facebook.com/");
     }
 }
