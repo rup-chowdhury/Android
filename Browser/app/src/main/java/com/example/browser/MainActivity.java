c5a23fb5a97837b0b9bec116c6a7c6155c458442
Rup Chowdhury <rupbd1@gmail.com>
2024-04-21 20:18:44 +0600

    Add editText and Button and handshake with Java

--- Browser/app/src/main/java/com/example/browser/MainActivity.java
+++ Browser/app/src/main/java/com/example/browser/MainActivity.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView viewBrowser;
+
+    EditText edtURL;
+
+    Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtURL = findViewById(R.id.edt_url);
+
+        btnGo = findViewById(R.id.btn_go);
+
         viewBrowser = findViewById(R.id.web_browser);
-        viewBrowser.loadUrl("https://www.facebook.com/");
+        viewBrowser.loadUrl("https://www.youtube.com/");
+        viewBrowser.getSettings().setJavaScriptEnabled(true);
+        viewBrowser.setWebViewClient(new WebViewClient());
     }
 }
