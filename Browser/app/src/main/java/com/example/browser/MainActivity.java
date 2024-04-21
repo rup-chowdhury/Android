72f38e406d665067d61d799a135ac88537927385
Rup Chowdhury <rupbd1@gmail.com>
2024-04-21 18:16:12 +0600

    a

--- Browser/app/src/main/java/com/example/browser/MainActivity.java
+++ Browser/app/src/main/java/com/example/browser/MainActivity.java
@@ -7,14 +7,14 @@ import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView Browser;
+    WebView viewBrowser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Browser = findViewById(R.id.web_browser);
-        Browser.loadUrl("https://www.facebook.com/");
+        viewBrowser = findViewById(R.id.web_browser);
+        viewBrowser.loadUrl("https://www.facebook.com/");
     }
 }
