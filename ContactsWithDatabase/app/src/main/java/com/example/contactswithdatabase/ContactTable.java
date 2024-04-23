6b494a05fe2346963daa3d082a0d0f6d1ed3e09c
Rup Chowdhury <rupbd1@gmail.com>
2024-04-23 21:07:48 +0600

    Add functions for CRUD operation

---
+++ ContactsWithDatabase/app/src/main/java/com/example/contactswithdatabase/ContactTable.java
@@ -0,0 +1,20 @@
+package com.example.contactswithdatabase;
+
+public class ContactTable {
+
+    private void insertContact(){
+
+    }
+
+    private void readContact(){
+
+    }
+
+    private void updateContact(){
+
+    }
+
+    private void deleteContact(){
+
+    }
+}
