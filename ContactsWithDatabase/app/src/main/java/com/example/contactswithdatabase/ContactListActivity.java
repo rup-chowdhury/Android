8dd988bd9f9797a71120ee78834cfd674870a51d
Rup Chowdhury <rupbd1@gmail.com>
2024-04-30 14:02:25 +0600

    Getting data into allContacts variable

--- ContactsWithDatabase/app/src/main/java/com/example/contactswithdatabase/ContactListActivity.java
+++ ContactsWithDatabase/app/src/main/java/com/example/contactswithdatabase/ContactListActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ContactListActivity extends AppCompatActivity {
 
     ListView lstContacts;
+
+    ArrayList<ContactModel> allContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,12 @@ public class ContactListActivity extends AppCompatActivity {
 
         lstContacts = findViewById(R.id.lst_contacts);
 
-        
+
+        ContactTable ct = new ContactTable(ContactListActivity.this);
+
+
+
+        allContacts = ct.getAllContacts();
+
     }
 }
