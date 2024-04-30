a3757c8538160b94106a4f6fd0a4a69d9895f4d3
Rup Chowdhury <rupbd1@gmail.com>
2024-05-01 01:11:43 +0600

    Add intent to go to the Contact List page

--- ContactsWithDatabase/app/src/main/java/com/example/contactswithdatabase/MainActivity.java
+++ ContactsWithDatabase/app/src/main/java/com/example/contactswithdatabase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.contactswithdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
                     ct.insertContact(cm);
 
                     Toast.makeText(MainActivity.this, "Contact Added", Toast.LENGTH_SHORT).show();
+
+
+                    startActivity(new Intent(MainActivity.this, ContactListActivity.class));
                 }
             }
         });
