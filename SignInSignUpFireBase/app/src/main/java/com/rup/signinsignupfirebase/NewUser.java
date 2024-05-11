b6d6744e5ac7d351b09bd8b93b876ac646dcecdd
Rup Chowdhury <rupbd1@gmail.com>
2024-05-11 23:14:34 +0600

    .

--- SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
+++ SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
@@ -1,12 +1,16 @@
 package com.rup.signinsignupfirebase;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewUser extends AppCompatActivity {
 
+    EditText edtSignUpEmail, edtSignUpPassword;
+
+    Button btnSignUp, btnSignIn;
 
 
     @Override
@@ -14,6 +18,13 @@ public class NewUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_user);
 
+        edtSignUpEmail = findViewById(R.id.edt_signup_email);
+        edtSignUpPassword = findViewById(R.id.edt_signup_password);
+
+        btnSignUp = findViewById(R.id.btn_sign_up);
+        btnSignIn = findViewById(R.id.log_in);
+
+        
 
     }
 }
