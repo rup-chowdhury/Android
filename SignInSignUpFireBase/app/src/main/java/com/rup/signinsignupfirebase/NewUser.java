3e7a9ca95ed1d945895dae2c8ef8811fe919523c
Rup Chowdhury <rupbd1@gmail.com>
2024-05-11 22:48:21 +0600

    Create new class

---
+++ SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
@@ -0,0 +1,14 @@
+package com.rup.signinsignupfirebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_user);
+    }
+}
