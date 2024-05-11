df6d7cb43070e3ae679f0e63c96fb81aebf78806
Rup Chowdhury <rupbd1@gmail.com>
2024-05-11 23:16:01 +0600

    Update NewUser.java

--- SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
+++ SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
@@ -1,6 +1,8 @@
 package com.rup.signinsignupfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,7 +26,12 @@ public class NewUser extends AppCompatActivity {
         btnSignUp = findViewById(R.id.btn_sign_up);
         btnSignIn = findViewById(R.id.log_in);
 
-        
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NewUser.this, MainActivity.class);
+            }
+        });
 
     }
 }
