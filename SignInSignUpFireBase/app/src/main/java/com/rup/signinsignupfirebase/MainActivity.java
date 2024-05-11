7f0731112ed4ed49bf4ebb4cd179a42333a80832
Rup Chowdhury <rupbd1@gmail.com>
2024-05-11 22:54:18 +0600

    finding elements by id

--- SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/MainActivity.java
+++ SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/MainActivity.java
@@ -3,12 +3,23 @@ package com.rup.signinsignupfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtSignInEmail, edtSignInPassword;
+
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtSignInEmail = findViewById(R.id.edt_signin_email);
+        edtSignInPassword = findViewById(R.id.edt_signin_password);
+
+        btnSignUp = findViewById(R.id.btn_sign_in);
     }
 }
--- SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
+++ SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
@@ -1,14 +1,19 @@
 package com.rup.signinsignupfirebase;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewUser extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_user);
+
+
     }
 }
