997713cce348790359e5a763993897af0989c88e
Rup Chowdhury <rupbd1@gmail.com>
2024-05-12 23:20:52 +0600

    Update NewUser.java

--- SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
+++ SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/NewUser.java
@@ -6,8 +6,16 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class NewUser extends AppCompatActivity {
 
@@ -15,11 +23,14 @@ public class NewUser extends AppCompatActivity {
 
     Button btnSignUp, btnSignIn;
 
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_user);
+
+        mAuth = FirebaseAuth.getInstance();
 
         edtSignUpEmail = findViewById(R.id.edt_signup_email);
         edtSignUpPassword = findViewById(R.id.edt_signup_password);
@@ -78,15 +89,16 @@ public class NewUser extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(NewUser.this, "Authentication successful.",
+                                    Toast.LENGTH_SHORT).show();
+
                             FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+
+                            Toast.makeText(NewUser.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+
                         }
                     }
                 });
