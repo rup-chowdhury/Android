9cc3aedb8216c76266ae0cbe80451eee09c09d6d
Rup Chowdhury <rupbd1@gmail.com>
2024-05-13 11:47:03 +0600

    Java setup for logout

--- SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/Info.java
+++ SignInSignUpFireBase/app/src/main/java/com/rup/signinsignupfirebase/Info.java
@@ -3,12 +3,33 @@ package com.rup.signinsignupfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Info extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    TextView txtEmail;
+
+    Button btnLogOut;
+
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        txtEmail = findViewById(R.id.txt_email);
+
+        btnLogOut = findViewById(R.id.btn_log_out);
+
+        
     }
 }
